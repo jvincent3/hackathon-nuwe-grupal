55778cc4d176ff6995f6ce99bb43e3d75f4816ec
Jvincent3 <johnvuc@hotmail.com>
2021-05-29 20:26:36 +0200

    Made the page mobile responsive

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-import {Box, Avatar, Text, Flex} from '@chakra-ui/react'
+import {Box, Avatar, Text, Flex, useMediaQuery} from '@chakra-ui/react'
 import {useGetGithubUser} from 'hooks/queries/githubuser'
 import RepoList from './RepoList'
 import Loading from 'components/Loading'
@@ -10,6 +10,7 @@ function User() {
 
     const {username} = useParams()
     const {isLoading, error, data} = useGetGithubUser({params: {username: username}})
+    const [isLargerThan600] = useMediaQuery("(min-width: 600px)")
 
     if ( isLoading ) return <Loading/>
 
@@ -19,30 +20,46 @@ function User() {
         <>
         {( !isLoading ) &&
             <>
-            { !data.message ?
                 <Box pt="100px" px="10px">
                     <Box maxWidth="800px" mx="auto">
+                        {isLargerThan600 ? 
+                        //User profile card for tablets and desktop
                         <Flex mb="20px" p="20px" borderRadius="5px" backgroundColor="#222a3a" justifyContent="space-between">
                             <Flex>
                                 <Box alignSelf="center">
-                                    <Avatar size="lg" src={data.avatar_url}/>
+                                    <Avatar size="2xl" src={data.avatar_url}/>
                                 </Box>
-                                <Box alignSelf="center" pl="10px">
-                                    <Text fontSize="3xl">{data.login}</Text>
+                                <Box alignSelf="center" pl="20px">
+                                    <Text fontSize="3xl">{data.name}</Text>
+                                    <Text fontSize="xl">{data.login}</Text>
+                                </Box>
+                            </Flex>
+                            <Box alignSelf="center">
+                                <Text fontSize="2xl">Total Repos: {data.public_repos}</Text>
+                            </Box>
+                        </Flex>
+                        :
+                        //User profile card for mobile
+                        <Box mb="20px" p="20px" borderRadius="5px" backgroundColor="#222a3a" >
+                            <Flex justifyContent="space-between">
+                                <Box alignSelf="center">
+                                    <Avatar size="xl" src={data.avatar_url}/>
+                                </Box>
+                                <Box alignSelf="center" pl="20px">
+                                    <Text textAlign="right" fontSize="xl">{data.name}</Text>
+                                    <Text textAlign="right" fontSize="md">{data.login}</Text>
                                 </Box>
                             </Flex>
                             <Box>
-                                <Text>Total Repos: {data.public_repos}</Text>
+                                <Text textAlign="right" fontSize="xl">Total Repos: {data.public_repos}</Text>
                             </Box>
-                        </Flex>
-                        <Box p="20px" borderRadius="5px" backgroundColor="#222a3a">
+                        </Box>
+                        }
+                        <Box p={{base:"10px", md: "20px"}} borderRadius="5px" backgroundColor="#222a3a">
                             <RepoList userData={data}/>
                         </Box>
                     </Box>
                 </Box>
-                :
-                <UserNotFound/>
-            }
             </>
         }
         </>
