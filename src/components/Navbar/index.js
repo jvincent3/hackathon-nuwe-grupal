46f3a1f78f95bc0a6572e80499252a54286a5741
Jvincent3 <johnvuc@hotmail.com>
2021-05-29 20:33:07 +0200

    Implemented new linnks

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Box, Text} from '@chakra-ui/react' 
+import {Flex, Box, Text} from '@chakra-ui/react' 
 
 
 function Navbar() {
     return (
-        <Box minHeight="80px" p="20px">
-            <Text fontWeight="semibold" fontSize="2xl"><Link to="/">Home</Link></Text>
-        </Box>
+        <Flex minHeight="80px" p="20px" justifyContent="space-between" boxShadow="0px 0px 1px 0px #ffffff">
+            <Box>
+                <Text fontWeight="semibold" fontSize="2xl"><Link to="/">Home</Link></Text>
+            </Box>
+            <Flex>
+                <Box px="10px">
+                    <Text fontWeight="semibold" fontSize="2xl"><Link to="/login">Login</Link></Text>
+                </Box>
+                <Box px="10px">
+                    <Text fontWeight="semibold" fontSize="2xl"><Link to="/register">Register</Link></Text>
+                </Box>
+            </Flex>
+        </Flex>
     )
 }
 
