a42015d2a0d24d6ce26ae36f664fe387517890b6
Sstark97 <aitorscinfo@gmail.com>
2021-05-30 12:19:08 +0100

    Landing page updated

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import {Grid, Box, Text} from '@chakra-ui/react'
+import {Grid, Box, Text, Image} from '@chakra-ui/react'
 import SearchGithubUser from 'components/SearchGithubUser'
 
 function Landing() {
     return (
         <Grid height="calc(100vh - 100px)" mx="auto">
-            <Box alignSelf="center" px="10px" height="calc(100vh - 150px)" >
-                <Box py="20px">
-                    {/* <Text textAlign="center" fontSize="3xl">Search a <Text as="span" fontWeight="semibold">Github</Text> user</Text> */}
-                </Box>
+            <Box display="flex" px="10px" height="calc(100vh - 150px)" marginLeft="5rem" marginBottom="5rem">
+            
+            <Box display="flex" flexDirection="column" justifyContent="center" alignSelf="center" px="10px" height="calc(100vh - 150px)" marginLeft="5rem" marginBottom="5rem">
+                    <Text fontWeight="extrabold" fontSize="6xl" width="container.md">The Future</Text>
+                    <Text fontWeight="extrabold" fontSize="6xl" width="container.md">of GitHub</Text>
+                    <Text fontWeight="light" fontSize="4xl">at present</Text>
+                    
+                    <Text marginTop="3rem" fontWeight="hairline" fontSize="large">
+                            Our mission is to take the sharing of 
+                    </Text>
+                    <Text fontWeight="hairline" fontSize="large">
+                            experiences and develop in teams to
+                    </Text>
+                    <Text fontWeight="hairline" fontSize="large">
+                            the next level.
+                    </Text>
+            </Box>
+
+            <Box display="flex" alignItems="center" px="10px" height="calc(100vh - 150px)" marginLeft="5rem" marginBottom="5rem" marginRight="5rem">
+
+                <Image src={require("assets/images/landing.svg")?.default}/>
+                
+            </Box>    
+
             </Box>
         </Grid>
     )
